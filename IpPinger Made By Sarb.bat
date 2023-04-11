5989c2b0251b165210fe37ccfe152478fe96e634
Sarb <100166055+MadnessFPS@users.noreply.github.com>
2023-04-11 11:41:29 +0200

    Add files via upload

---
+++ IpPinger Made By Sarb.bat
@@ -0,0 +1,130 @@
+@echo off
+:a
+mode 100,50
+color C
+
+title Custom IP Pinger With Your Name : Made by @Sarb#6666
+
+:greeting
+cls
+
+echo                                                                                       
+echo                                            ~77!~^::::::~J7::::^~^^~!!^                              
+echo                                            Y#BGGP55PBP55P55PG5PGPGGGGJ                              
+echo                                               ^!!~::..~B7::^^:7G!^:..:::.                              
+echo        .JJJJJJJJJJJJJYYYYYYYYYYYYYY5YYYYYYYYYYYYYYYYYYYGGGPYYYY5GP5JJ?.                                 
+echo        .~~~~~~~~~~~~~~~~~!!!!!!!!!!75P5P5555PP555555555YYYYYYYYYYJJ??J?7!~^^~???77777???????????Y5:     
+echo                               .?5GBBBG7!!!!!77777?777???????YYJJYYYYYYYJJY5JJ??7777777!!!!!5G:     
+echo                               .~?YGP?~                      ^:.:~..^?PG5?5GGGGBGGGPP555YYYJGG:     
+echo                                  ?BY~                        ...     :YPJ7!:^!7?Y5PGGGBGGGPBG.     
+echo                                 .PBJ                                   .          .:^~7PY5PBG.     
+echo                                 JYG^                                                   !. .:.      
+echo                                !GP#:                                                               
+echo                               ^GY?#~                                                               
+echo                               !B?.^.                                                               
+echo                               :~:                                                           
+                                                                     
+echo           #-----------------# #-----------#
+echo           # @Sarb#6666 # # IP PINGER #
+echo           #-----------------# #-----------#
+echo !Name doesnt get logged just for a better experience!
+set /p Name= Enter A Name:
+title Oi %Name% Target=
+set /p IP= Smack Ip here %Name%:
+title Oi %Name% Target= %IP%
+echo Target = %IP%
+echo RIP WIFI ----%IP% -- APOLLO OP
+title WiFi eat shit nerd
+:top
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been taken down...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 01
+echo -- %Name% OP --
+title M
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+echo -- %Name% OP --
+title MA
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+echo -- %Name% OP --
+title MAD
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+echo -- %Name% OP --
+title MADN
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+echo -- %Name% OP --
+title MADNE
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+echo -- %Name% OP --
+title MADNES
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+echo -- %Name% OP --
+title MADNESS
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+echo -- %Name% OP --
+title MADNESSF
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+echo -- %Name% OP --
+title MADNESSFP
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+echo -- %Name% OP --
+title MADNESSFPS
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+echo -- %Name% OP --
+title MADNESSFPS :
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+echo -- %Name% OP --
+title MADNESSFPS :)
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+echo -- %Name% OP --
+title MADNESSFPS :) <3
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+echo -- %Name% OP --
+title MADNESSFPS :) <3 EZ
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+echo -- %Name% OP --
+title Get fucked
+echo Made by @Sarb#6666
+echo %Name% going for %IP% whos gonna get fucked
+GoTo top
